fa605f98880f0d856018743538c3ffe4fb92fe31
atishayjain2003 <atishayjain2003@gmail.com>
2024-10-11 00:30:35 +0530

    Creating and defining models

--- mysite/movies/admin.py
+++ mysite/movies/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Moviedata
+admin.site.register(Moviedata)
 # Register your models here.
--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Moviedata(models.Model):
+    name=models.CharField(max_length=200)
+    duration = models.FloatField()
+    rating = models.FloatField()
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
